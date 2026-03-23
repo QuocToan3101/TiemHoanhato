dbab58b36d06663bca4e2bedd2d61048f9d47ed2
MikeOlala <23130184@st.hcmuaf.edu.vn>
2026-03-23 15:07:23 +0700

    fix: sua lai chuc nang add tai admin

---
+++ src/main/java/controller/BaseServlet.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class BaseServlet extends HttpServlet {
+    protected String getAction(HttpServletRequest res) {
+        String path = res.getPathInfo();
+        if (path != null || path.equals("/")) return "index";
+        return path;
+    }
+    protected void json(HttpServletResponse resp, String json) throws IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(json);
+}
+}
